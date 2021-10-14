1b0418c18b6cb870849aaf1d382b263464e73f75
allyboy08 <alexsassman65@gmail.com>
2021-10-14 14:49:22 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,54 @@
 <template>
   <h1>{{title}}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
+  <p>enter</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="village" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign</a>
+        <a href="#">more</a>
+      </template>
+      <h1>giveaway</h1>
+      <p>grab your ninja headband for a coin</p>
+    </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>newsletter</h1>
+      <p>updates</p>
+    </Modal>
+  </teleport>
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open modal</button>
 </template>
 
 <script>
+import Modal from './components/Modal'
+
+
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
-      title: 'my first vue app'
+      title: 'my first vue app',
+      showModal: false,
+      showModalTwo: false
     }
   },
   methods: {
-    handleClick() {
-      cosole.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   }
 }
 </script>
 
 <style>
-#app {
+#app .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
